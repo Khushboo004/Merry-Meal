8f000f8981d4b3a54a0f5ea0ea2cf58012d5fd94
Harris <aungthiha12345mdy@gmail.com>
2023-02-02 16:56:42 +0630

    reconfigure

--- src/services/MealService.js
+++ src/services/MealService.js
@@ -1,8 +1,4 @@
 const getAllMeals = () => {
   const token = localStorage.getItem("token");
-  return axios.post("/api/v1/partners/meals", {
-    headers: {
-      Authorization: "Bearer " + token,
-    },
-  });
+  return axios.post("/api/v1/partners/meals");
 };
